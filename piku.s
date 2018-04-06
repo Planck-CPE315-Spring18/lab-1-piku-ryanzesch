1c0e2b1b23fc47fb600a95cdb2efcd99f82e9efa
ryanzesch <ryan.zesch@gmail.com>
2018-04-06 15:13:41 -0700

    Update piku.s

--- piku.s
+++ piku.s
@@ -8,17 +8,25 @@ main:
     push    {ip, lr}
 
     @ Load the argument and perform the call. This is like 'printf("...")' in C.
-    ldr     r0, =message
+    ldr     r0, =line1
+    bl      printf
+    ldr     r0, =line2
+    bl      printf
+    ldr     r0, =line3
     bl      printf
 
     @ Exit from 'main'. This is like 'return 0' in C.
     mov     r0, #0      @ Return 0.
     @ Pop the dummy ip to reverse our alignment fix, and pop the original lr
-    @ value directly into pc — the Program Counter — to return.
+    @ value directly into pc â the Program Counter â to return.
     pop     {ip, pc}
 
     @ --------------------------------
     @ Data for the printf call. The GNU assembler's ".asciz" directive
     @ automatically adds a NULL character termination.
-message:
-    .asciz  "Hello, world.\n"
+line1:
+    .asciz  "This is my pi-ku.\n"
+line2:
+    .asciz  "It is being made in vim.\n"
+line3:
+    .asciz  "On raspberry pi.\n"
